60e72e4d81ffc5850af2807256531a2850d4cb76
Faisal Orakzai <imorakzai1122@gmail.com>
2026-05-17 08:14:34 +0000

    feat: wire VITE_API_BASE_URL to API client via setBaseUrl

--- artifacts/shamim-forever/src/main.tsx
+++ artifacts/shamim-forever/src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
+  import { setBaseUrl } from "@workspace/api-client-react";
+  import App from "./App";
+  import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+  const apiBase = import.meta.env.VITE_API_BASE_URL ?? "";
+  if (apiBase) setBaseUrl(apiBase);
+
+  createRoot(document.getElementById("root")!).render(<App />);
+  
