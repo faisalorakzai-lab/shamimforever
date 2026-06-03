c9dbb24e659776aa5886494e859bd88fa2e44f68
Faisal Orakzai <imorakzai1122@gmail.com>
2026-06-03 16:46:44 +0000

    seo: add robots.txt pointing Google to sitemap

---
+++ app/robots.ts
@@ -0,0 +1,16 @@
+import { MetadataRoute } from 'next'
+
+  export default function robots(): MetadataRoute.Robots {
+    return {
+      rules: [
+        {
+          userAgent: '*',
+          allow: '/',
+          disallow: ['/api/', '/admin/'],
+        },
+      ],
+      sitemap: 'https://shamimforever.com/sitemap.xml',
+      host: 'https://shamimforever.com',
+    }
+  }
+  
